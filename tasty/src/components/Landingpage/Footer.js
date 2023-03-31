127976a904d7cab2293bf4dfde764e89cdbbced0
the-datageek <moraaf770@gmail.com>
2023-03-31 12:23:15 +0300

    Slight changes

---
+++ tasty/src/components/Landingpage/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer style={footerStyle}>
+      <div style={socialMediaStyle}>
+        <a href="https://www.facebook.com"><FaFacebook size={20} /></a>
+        <a href="https://www.twitter.com"><FaTwitter size={20} /></a>
+        <a href="https://www.instagram.com"><FaInstagram size={20} /></a>
+      </div>
+      <div style={copyright}>
+        © 2023 Tasty Trail. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+const footerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  backgroundColor: 'black',
+  color: 'orange',
+  padding: '2rem',
+  marginTop: '2rem',
+};
+
+const socialMediaStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const copyright = {
+  fontSize: '0.8rem',
+  padding: '5px'
+};
+
+export default Footer;
