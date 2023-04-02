33e8d1cc809b24147453e9c3f01d31aa24f05744
the-datageek <moraaf770@gmail.com>
2023-04-02 20:10:11 +0300

    Fixed merge conflicts

--- tasty/src/App.js
+++ tasty/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import About from "./components/Landingpage/About/About"
 import Contacts from "./components/Landingpage/Contact/Contacts";
@@ -10,20 +9,16 @@ import Meal from "./components/Home/Home";
 import { Myrecipe } from "./components/MyRecipe/myrecipe";
 import { Profile } from "./components/Profile/Profile";
 import {Routes, Route} from 'react-router-dom'
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> main
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
 
-      <>
+
+      <div>
         <Navbar />
         <div>
-            <Routes>
+          <Routes>
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/recipes" element={<Meal/>}/>
                 <Route path="/myrecipes" element={<Myrecipe/>}/>
@@ -37,35 +32,12 @@ function App() {
         {/* <Footer /> */}
         </div>
          
-
-      {/* <Navbar />
-      <LandingPage />
-      <About/>
-      <Contacts />
-      <Footer /> */}
-      {/* <Meal /> */}
-      {/* <Myrecipe/>
-       */}
-      {/* <Profile /> */}
-      </>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> main
+      </div>
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
--- tasty/src/index.js
+++ tasty/src/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'; 
-=======
->>>>>>> main
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
-        <App />
+       <App/>
     </BrowserRouter>
     
-=======
-    <App />
->>>>>>> main
   </React.StrictMode>
 );
 
