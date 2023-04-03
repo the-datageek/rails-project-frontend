b5ab47b4239fb6486d9ce6dd5e2c63c3466db772
the-datageek <moraaf770@gmail.com>
2023-04-03 12:15:23 +0300

    Code cleanup

--- tasty/src/components/Landingpage/Contact/Contacts.js
+++ tasty/src/components/Landingpage/Contact/Contacts.js
@@ -6,25 +6,25 @@ const contacts = [
   {
     id: 1,
     name: "Ernest Kipchumba",
-    image: "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-6/271847244_650286522826602_207207098764189920_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGIkt7NlvQCnBftvuc0ONmt4WJHZZRKaJHhYkdllEpokSkNbAodRSmoyuVdjavMpWEk8YbfHMvnfJxYDlk4Qdc4&_nc_ohc=d-cc3LiQEhQAX-lkF4G&_nc_ht=scontent.fnbo8-1.fna&oh=00_AfC9fZ1VXAt-Lsy1EJRcvTjljsZITYAI-14xDgFsL5dr_w&oe=6428F0B0",
+    image: "https://i.pinimg.com/236x/ec/2c/3d/ec2c3d6d4de6e36f5db171c31b730e8a.jpg",
     details: "Frontend Developer"
   },
   {
     id: 2,
     name: "Faith Moraa",
-    image: "https://i.pinimg.com/236x/0f/b9/76/0fb9764e54601ddaf201a14df9abc867.jpg",
+    image: "https://i.pinimg.com/236x/d7/4f/da/d74fda5f6ff2020d7633e07dc8733edf.jpg",
     details: "Frontend Developer"
   },
   {
     id: 3,
     name: "Hassan Hassan",
-    image: "https://i.pinimg.com/236x/df/94/84/df94842a9cc5a5b3de069f4c281a42f7.jpg",
+    image: "https://i.pinimg.com/236x/c0/0a/8c/c00a8ca98c5a895aee9e9fd3584860ed.jpg",
     details: "Backend Developer"
   },
   {
     id: 4,
     name: "Faith Kigen",
-    image: "https://i.pinimg.com/236x/7b/da/06/7bda060e4ecc6c0fe0a384a9341b63a4.jpg",
+    image: "https://i.pinimg.com/236x/2b/52/4e/2b524e336c16d5defec2f8cc532c765b.jpg",
     details: "Backend Developer"
   }
 ];
